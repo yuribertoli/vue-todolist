bb90e27d523ec6a6088f2e6c4c89146d129f17f2
yuribertoli <yuribertoli@yahoo.it>
2022-02-11 14:33:48 +0100

    1

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = new Vue ({
     el: "#root",
 
     data: {
-
+        input: "",
+        toDoList: ["Studiare la documentazione", "Non capirci niente", "Ristudiare la documentazione", "Rinoncapirci niente"]
     },
 
     methods: {
