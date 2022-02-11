9e6e0f313ad4342c5ec3804bb0133f47f1605a72
yuribertoli <yuribertoli@yahoo.it>
2022-02-11 19:33:30 +0100

    1

--- Soluzione 1/main.js
+++ Soluzione 1/main.js
@@ -42,13 +42,16 @@ const app = new Vue ({
     el: "#root",
 
     data: {
+
+        //creo una variabile vuota per popolare il v-model dell'input
         input: "",
+
+        //creo un array di oggetti dove inserisco la frase da mostrare e la variabile utile al toggle
         toDoList: [ {frase: "Studiare la documentazione", done: false}, 
                     {frase: "Non capire la documentazione", done: false}, 
                     {frase: "Ristudiare la documentazione", done: false}, 
                     {frase: "Rinoncapire la documentazione", done: false} 
                 ],
-        /* striked: "" */
     },
 
     methods: {
@@ -56,8 +59,10 @@ const app = new Vue ({
         add() { 
             if (this.input != "") { //evito di aggiungere valori vuoti alla lista
 
+                //creo una variabile dove inserisco un oggetto con frase i valori della variabile input
                 let oggetto = {frase: this.input, done: false}
-                //aggiungo all'array il valore della variabile input
+
+                //aggiungo all'array l'oggetto creato
                 this.toDoList.push(oggetto);
 
                 //resetto il valore
@@ -65,10 +70,12 @@ const app = new Vue ({
             }       
         },
 
+        //utilizzo splice per rimuovere all'indice selezionato 1 elemento (quello selezionato quindi)
         remove(index) {
             this.toDoList.splice(index, 1);
         },
 
+        //toggle per cambiare valore di done da false a true e viceversa
         active(indice) {
 
             this.toDoList[indice].done = !this.toDoList[indice].done
